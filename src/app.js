477b943daaffedd5631693984cb22dec2859f854
Olena Lutsyuk <elaiyne.nik@gmail.com>
2022-12-16 03:06:32 +0200

    Add real data for the city, temperature and description

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-alert("Hello World!");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "8f38c6e1335be5316719a809c492c27b";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Petah Tikva&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
