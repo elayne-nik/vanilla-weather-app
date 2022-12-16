a1596d5ca0eaf584a4de04ed3d25229aa466c338
Olena Lutsyuk <elaiyne.nik@gmail.com>
2022-12-16 23:31:25 +0200

    Showing current humidity and wind

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windspeedElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "8f38c6e1335be5316719a809c492c27b";
