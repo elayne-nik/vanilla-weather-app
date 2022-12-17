06d899cfea7b4e484646850ae8c25f355ed670d5
Olena Lutsyuk <elaiyne.nik@gmail.com>
2022-12-17 02:27:25 +0200

    Added search engine

--- src/app.js
+++ src/app.js
@@ -42,8 +42,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "8f38c6e1335be5316719a809c492c27b";
-let city = "Petah Tikva";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "8f38c6e1335be5316719a809c492c27b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Petah Tikva");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
